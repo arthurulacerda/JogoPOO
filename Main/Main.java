ec77aad2f1bb19c21f257bcaf6424a5bedc2a37b
kayquesousa <kayque23@hotmail.com>
2015-05-12 23:13:43 -0300

    Create Main.java

---
+++ Main/Main.java
@@ -0,0 +1,21 @@
+package Main;
+
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+
+    public static void main(String[] args)  {
+
+        JFrame window = new JFrame("Jogo");
+        window.setContentPane(new GamePanel());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.pack();
+        
+        window.setVisible(true);  
+    }
+
+}
